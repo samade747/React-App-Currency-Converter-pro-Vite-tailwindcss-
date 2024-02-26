77d8fec19a5562fd25e2d2d6a834d476a0c80465
samade747 <samad.e747@gmail.com>
2024-02-26 14:45:47 +0500

    Update App.jsx

--- currenyconverter/src/App.jsx
+++ currenyconverter/src/App.jsx
@@ -10,8 +10,8 @@ function App() {
     <div className='flex flex-col items-center justify-center p-10 w-full h-screen'>
  
       <div className='bg-gray-200 border-2 border-gray-400 rounded-lg p-2'>
-        <h1 className='bg-red-600 text-white font-bold w-full text-center py-4 px-2 rounded-lg text-lg'>Curreny Converter</h1>
-          <div className='bg-red-600 text-white font-bold text-center w-full py-4 px-2 rounded-lg text-lg'>
+        <h1 className='bg-red-600 gap-5 text-white font-bold w-full text-center py-4 px-2 rounded-lg text-lg'>Curreny Converter</h1>
+          <div className='bg-red-600 text-white font-bold text-center w-full py-4 px-2 rounded-lg text-lg gap-4'>
             <h6 className='="italic text-blue-600 text-lg font-semibold border-b-2 border-blue-600"'>Currency Options</h6>
               <p className='text-base font-medium m-0'>Value to convert </p>
                 <input type="text" className='w-150'/>
