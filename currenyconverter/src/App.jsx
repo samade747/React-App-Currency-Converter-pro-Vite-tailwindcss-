715d086f2343777288f5ff9dbf6d8728a5c561a1
samade747 <samad.e747@gmail.com>
2024-02-26 14:57:57 +0500

    257pm 26 2 2024

--- currenyconverter/src/App.jsx
+++ currenyconverter/src/App.jsx
@@ -10,17 +10,17 @@ function App() {
     <div className='flex flex-col items-center justify-center p-10 w-full h-screen'>
  
       <div className='bg-gray-200 border-2 border-gray-400 rounded-lg p-2'>
-        <h1 className='bg-red-600 gap-5 text-white font-bold w-full text-center py-4 px-2 rounded-lg text-lg'>Curreny Converter</h1>
-          <div className='bg-red-600 text-white font-bold text-center w-full py-4 px-2 rounded-lg text-lg gap-4'>
-            <h6 className='="italic text-blue-600 text-lg font-semibold border-b-2 border-blue-600"'>Currency Options</h6>
+        <h1 className='bg-red-600 gap-5 text-white font-bold w-full text-center py-4 px-2 rounded-lg text-lg'>Currency Converter</h1>
+          <div className='bg-red-600 text-white font-bold text-center w-full py-4 px-2 rounded-lg text-lg m-1'>
+            <h6 className="italic text-blue-600 text-lg font-semibold border-b-2 border-blue-600 m-1">Currency Options</h6>
               <p className='text-base font-medium m-0'>Value to convert </p>
-                <input type="text" className='w-150'/>
+                <input type="text" className='w-150 text-black'/>
                   <div className='flex flex-col w-full gap-4'>
                       <div className='flex'>
                         <label htmlFor="rupees1">from                          
                          
                           <select className='text-black' name="" id="rupees1" style={{width: "100%"}}>
-                            <option value="">select curreny</option>
+                            <option value="">select currency</option>
                               <option value="pkr">pakistan</option>
                               <option value="usa">USA</option>
                           </select>
@@ -32,7 +32,7 @@ function App() {
                       <div className='flex'>
                         <label htmlFor="rupees1">To                          
                           <select className='text-black' name="" id="rupees2" style={{width: "100%"}}>
-                            <option value="">select curreny</option>
+                            <option value="">select currency</option>
                               <option value="pkr">pakistan</option>
                               <option value="usa">USA</option>
                           </select>
